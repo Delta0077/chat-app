fd3bb318625936e326bdafce5f50d6ff1b4ec504
Delta0077 <adas0262@gmail.com>
2025-01-12 15:15:32 +0530

    Importing 'HTTP' & 'CORS'.

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
+// cors and http import
+const http = require("http").Server.app();
+const cors = require("cors");
+
 app.get("./api", (req, res) => {
   res.json({
     message: "Hello World",
