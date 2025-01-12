5b886a1c3281f07c0dc072781c5163381046e987
Delta0077 <adas0262@gmail.com>
2025-01-12 23:53:30 +0530

    Setting up Socket.io

--- server/index.js
+++ server/index.js
@@ -6,6 +6,20 @@ const PORT = 4000;
 const http = require("http").Server.app();
 const cors = require("cors");
 
+// Importing Socket.io
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected`);
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+  });
+});
+
 app.get("./api", (req, res) => {
   res.json({
     message: "Hello World",
